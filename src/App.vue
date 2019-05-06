e50fae0a40db1af34fd2114fd2c3524e49584a47
Mia Laurea <mialaurea97@gmail.com>
2019-05-05 17:26:03 -0700

    Fix album data

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <div id="app">
     <h3> Vue Albums List </h3>
-    <!-- <p v-for="album in albums" :key="album.id"> {{album.title}} </p> -->
+    <!-- <p v-for="album in albums" :key="album.id"> {{album.title}} </p>  -->
+  
+    <!-- <div v-for="photo in photos" :key="photo.id"> -->
+        <table v-bind:colspan="5">
+          <thead>
+            <td> album ID </td>
+            <td> album title </td>
+            <td> photo title</td>
+            <td> photo thumbnail </td>
+          </thead>
+          <tr v-for="photo in photos" :key="photo.id">
+            <td> {{photo.albumId}}</td>
+              <td> 
+                {{ photo.albumId === 1 ? "quidem molestiae enim" : false }}
+                {{ photo.albumId === 2 ? "sunt qui excepturi placeat culpa" : false }}
+                {{ photo.albumId === 3 ? "omnis laborum odio" : false }}
+                {{ photo.albumId === 4 ? "non esse culpa molestiae omnis sed optio" : false }}
+                {{ photo.albumId === 5 ? "eaque aut omnis a" : false }}
+                {{ photo.albumId === 6 ? "natus impedit quibusdam illo est" : false }}
+                {{ photo.albumId === 7 ? "quibusdam autem aliquid et et quia" : false }}
+                {{ photo.albumId === 8 ? "qui fuga est a eum" : false }}
+                {{ photo.albumId === 9 ? "saepe unde necessitatibus rem" : false }}
+                {{ photo.albumId === 10 ? "distinctio laborum qui" : false }}
 
-    <table v-bind:colspan="5">
-    <!-- <tr v-for="photo in photos" :key="photo.id"> 
-      <td> <img v-bind:src="photo.url" v-bind:alt="photo.title"> </td> -->
-      <thead>
-        <td> album ID </td>
-        <td> album title </td>
-        <td> photo title</td>
-        <td> photo thumbnail </td>
-      </thead>
-      <tr v-for="album in albums" :key="album.id">
-        <td> {{album.userId}}</td>
-        <td> {{album.title}}</td>
-      </tr>
-      <!-- <tr v-for="photo in photos" :key="photo.id">
-        <td> {{photo.id}}</td>
-        <td> {{photo.title}}</td>
-      </tr> -->
-    </table>
-  </div>
+             </td>
+            <td> {{photo.title}} </td>
+            <td> <img src = photo.thumbnailUrl alt="photo"/> </td>
+          </tr>
+        </table>
+  </div> <!-- app div --> 
 </template>
 
 <script>
